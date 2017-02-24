20228c90bf77ec7a27a68ea82cede7cf9fe250bd
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-24 09:22:08 -0500

    Established connections between views and controller.

--- Factoring Buddy/AppDelegate.swift
+++ Factoring Buddy/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
 
 
+    @IBOutlet weak var a: NSTextField!
+    @IBOutlet weak var b: NSTextField!
+    @IBOutlet weak var c: NSTextField!
+    @IBOutlet weak var result: NSTextField!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
@@ -22,6 +27,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
+    @IBAction func factor(_ sender: Any) {
+        
+        print(a.intValue)
+        print(b.intValue)
+        print(c.intValue)
+    }
 
 }
 
