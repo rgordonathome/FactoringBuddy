09934b4b67077da43c8acd36217538dfe4d49766
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-24 08:45:09 -0500

    Initial Commit

---
+++ Factoring Buddy/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Factoring Buddy
+//
+//  Created by Russell Gordon on 2/24/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
